2a3a09a3c8a19666b9f16763df85fff5392174ad
Fred Snyder <fsnyder@castwide.com>
2022-02-12 07:00:36 -0500

    Test configurable shell

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -8,70 +8,65 @@ import { platform } from 'os';
 
 suite('rubySpawn', () => {
     it('spawns a Ruby process', (done) => {
-		let script = path.resolve('.', 'fixtures', 'cwd.rb');
-		let child = rubySpawn('ruby', [script]);
-		child.on('close', (code) => {
-			expect(code).to.equal(0);
-			done();
-		});
-	});
-
-	it('sets the current directory', (done) => {
-		let script = path.resolve('.', 'fixtures', 'cwd.rb');
-		let dir = path.resolve('.', 'fixtures');
-		let child = rubySpawn('ruby', [script], { cwd: dir });
-		let out = '';
-		child.stdout.on('data', (buffer) => {
-			out = out + buffer.toString();
-		});
-		child.stdout.on('close', () => {
-			expect(path.normalize(out)).to.equal(dir);
-			done();
-		});
-	});
-
-	it('sets a foreign directory', (done) => {
-		let script = path.resolve('.', 'fixtures', 'cwd.rb');
-		let dir = path.resolve('.');
-		let child = rubySpawn('ruby', [script], { cwd: dir });
-		let out = '';
-		child.stdout.on('data', (buffer) => {
-			out = out + buffer.toString();
-		});
-		child.stdout.on('close', () => {
-			expect(path.normalize(out)).to.equal(dir);
-			done();
-		});
-	});
-
-	if (platform().match(/darwin|linux/)) {
-		it('works with .ruby-version', (done) => {
-			// This test assumes that Ruby 2.4.6 is available via rvm/rbenv
-			let script = path.resolve('.', 'fixtures', 'version.rb');
-			let dir = path.resolve('.', 'fixtures', 'change');
-			let child = rubySpawn('ruby', [script], { cwd: dir });
-			let out = '';
-			child.stdout.on('data', (buffer) => {
-				out = out + buffer.toString();
-			});
-			child.stdout.on('close', () => {
-				expect(out).to.match(/2\.4\.6$/);
-				done();
-			});
+        let script = path.resolve('.', 'fixtures', 'cwd.rb');
+        let child = rubySpawn('ruby', [script]);
+        child.on('close', (code) => {
+            expect(code).to.equal(0);
+            done();
         });
-        
-        it('uses the specified shell', (done) => {
-			let script = path.resolve('.', 'fixtures', 'shell.rb');
-			let child = rubySpawn('ruby', [script], { shell: '/bin/sh' });
-			let out = '';
-			child.stdout.on('data', (buffer) => {
-				out = out + buffer.toString();
-			});
+    });
+
+    it('sets the current directory', (done) => {
+        let script = path.resolve('.', 'fixtures', 'cwd.rb');
+        let dir = path.resolve('.', 'fixtures');
+        let child = rubySpawn('ruby', [script], { cwd: dir });
+        let out = '';
+        child.stdout.on('data', (buffer) => {
+            out = out + buffer.toString();
+        });
+        child.stdout.on('close', () => {
+            expect(path.normalize(out)).to.equal(dir);
+            done();
+        });
+    });
+
+    it('sets a foreign directory', (done) => {
+        let script = path.resolve('.', 'fixtures', 'cwd.rb');
+        let dir = path.resolve('.');
+        let child = rubySpawn('ruby', [script], { cwd: dir });
+        let out = '';
+        child.stdout.on('data', (buffer) => {
+            out = out + buffer.toString();
+        });
+        child.stdout.on('close', () => {
+            expect(path.normalize(out)).to.equal(dir);
+            done();
+        });
+    });
+
+    if (platform().match(/darwin|linux/)) {
+        it('works with .ruby-version', (done) => {
+            // This test assumes that Ruby 2.4.6 is available via rvm/rbenv
+            let script = path.resolve('.', 'fixtures', 'version.rb');
+            let dir = path.resolve('.', 'fixtures', 'change');
+            let child = rubySpawn('ruby', [script], { cwd: dir });
+            let out = '';
+            child.stdout.on('data', (buffer) => {
+                out = out + buffer.toString();
+            });
             child.stdout.on('close', () => {
-                console.log(out);
-				expect(out).to.equal('/bin/sh');
-				done();
-			});
+                expect(out).to.match(/2\.4\.6$/);
+                done();
+            });
         });
-	}
+
+        it('uses the specified shell', (done) => {
+            let script = path.resolve('.', 'fixtures', 'shell.rb');
+            let child = rubySpawn('ruby', [script], { shell: '/bin/sh' });
+            child.stdout.on('close', () => {
+                expect(child['spawnfile']).to.equal('/bin/sh');
+                done();
+            });
+        });
+    }
 });
